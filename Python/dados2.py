26b1ccb1826be93db59c4a16737679bd17cc1fd3
Edson Tomas <edson.transpioneira@gmail.com>
2022-01-21 20:54:04 -0300

    menu adicionado

--- Python/dados2.py
+++ Python/dados2.py
@@ -15,8 +15,7 @@ def listar_categorias(dados):
     '''
     O parâmetro "dados" deve ser uma lista de dicionários representando os produtos.
     Essa função deverá retornar uma lista contendo todas as categorias dos diferentes produtos.
-    Cuidado para não retornar categorias repetidas.   
-    em cada função dar a opção de digitar 0 para voltar (Edson) 
+    Cuidado para não retornar categorias repetidas.    
     '''
     ...
 
@@ -25,7 +24,6 @@ def listar_por_categoria(dados, categoria):
     O parâmetro "dados" deve ser uma lista de dicionários representando os produtos.
     O parâmetro "categoria" é uma string contendo o nome de uma categoria.
     Essa função deverá retornar uma lista contendo todos os produtos pertencentes à categoria dada.
-    em cada função dar a opção de digitar 0 para voltar (Edson)
     '''
     ...
     
@@ -35,7 +33,6 @@ def produto_mais_caro(dados, categoria):
     O parâmetro "dados" deve ser uma lista de dicionários representando os produtos.
     O parâmetro "categoria" é uma string contendo o nome de uma categoria.
     Essa função deverá retornar um dicionário representando o produto mais caro da categoria dada.
-    em cada função dar a opção de digitar 0 para voltar (Edson)
     '''
     ...
 
@@ -45,7 +42,6 @@ def produto_mais_barato(dados, categoria):
     O parâmetro "dados" deve ser uma lista de dicionários representando os produtos.
     O parâmetro "categoria" é uma string contendo o nome de uma categoria.
     Essa função deverá retornar um dicionário representando o produto mais caro da categoria dada.
-    em cada função dar a opção de digitar 0 para voltar (Edson)
     '''
     ...
 
@@ -53,7 +49,6 @@ def top_10_caros(dados):
     '''
     O parâmetro "dados" deve ser uma lista de dicionários representando os produtos.
     Essa função deverá retornar uma lista de dicionários representando os 10 produtos mais caros.
-    em cada função dar a opção de digitar 0 para voltar (Edson)
     '''
     ...
 
@@ -61,33 +56,11 @@ def top_10_baratos(dados):
     '''
     O parâmetro "dados" deve ser uma lista de dicionários representando os produtos.
     Essa função deverá retornar uma lista de dicionários representando os 10 produtos mais caros.
-    em cada função dar a opção de digitar 0 para voltar (Edson)
-
     '''
     ...
 
-def imprimi_menu () :
-    print("Digite uma opção : ")
-    print("1. Listar categorias :")
-    print("2. Listar produtos de uma categoria :")
-    print("3. Produto mais caro por categoria :")
-    print("4. Produto mais barato por categoria :")
-    print("5. Top 10 produtos mais caros :")
-    print("6. Top 10 produtos mais baratos :")
-    print("0. Sair :")
-    opcao = input("Digite a opção desejada")
-    return opcao
-
 def menu(dados):
-    
-    opcao=imprimi_menu()
-    eUmaLetra = opcao.isdigit()
-    while opcao > 6 or opcao < 0 or eUmaLetra:
-        print("Opção inválida")
-        opcao=imprimi_menu()
-
-
-'''
+    '''
     O parâmetro "dados" deve ser uma lista de dicionários representando os produtos.
     Essa função deverá, em loop, realizar as seguintes ações:
     - Exibir as seguintes opções:
@@ -105,7 +78,10 @@ def menu(dados):
     - Imprimir o retorno salvo. 
     O loop encerra quando a opção do usuário for 0.
     '''
-    
+    ...
+
+
 # Programa Principal - não modificar!
 d = obter_dados()
+print(d)
 menu(d)
