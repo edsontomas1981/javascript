96b6e1642d7123348e9757ac22fd9386d70821da
edsontomas1981 <88893244+edsontomas1981@users.noreply.github.com>
2022-04-11 09:40:32 -0300

    Delete Comunidade digital directory

--- Comunidade digital/profissional.py
+++
@@ -1,5 +0,0 @@
-class Profissional:
-    def __init__(self,ramo:list,atividade:list):
-        self.ramo = ramo
-        self.atividade = atividade
-    
--- Comunidade digital/usuario.py
+++
@@ -1,8 +0,0 @@
-class Usuario:
-
-    def __init__(self,nome,email,endereco):
-        self.nome = nome
-        self.email = email
-        self.endereco = endereco
-        
-         
